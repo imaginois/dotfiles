81bbef92fd8e06b674c732b1866a6c97c608e0a8
Metodi Deribanov <imaginois@gmail.com>
2019-03-27 00:25:21 +0200

    add atom packages

--- scripts/atom.sh
+++ scripts/atom.sh
@@ -4,4 +4,14 @@ echo "### Setup Atom.io"
 echo "##  Install listed packages"
 
 apm install atom-ide-ui
+apm install ide-typescript
+apm install ide-php
+apm install ide-css
+apm install ide-json
+apm install atom-material-syntax
+apm install git-gui
+apm install git-plus
+apm install git-line
+apm install git-status
+apm install git-time-machine
 
