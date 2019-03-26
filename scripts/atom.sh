2c0f7f657767ace590ed600979bd7e09c4f46a68
Metodi Deribanov <imaginois@gmail.com>
2019-03-27 00:49:26 +0200

    atom.sh | add some more packages

--- scripts/atom.sh
+++ scripts/atom.sh
@@ -8,7 +8,15 @@ apm install ide-typescript
 apm install ide-php
 apm install ide-css
 apm install ide-json
+apm install atom-material-ui
 apm install atom-material-syntax
+apm install atom-material-syntax-dark
+apm install monokai
+apm install framer-ui
+apm install framerjs-syntax
+apm install framer-syntax
+apm install cold-ui
+apm install cold-syntax
 apm install git-gui
 apm install git-plus
 apm install git-line
